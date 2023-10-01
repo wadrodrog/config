c8e7ab80e1c59c540a8dc00feee251f8308e0392
wadrodrog <135006964+wadrodrog@users.noreply.github.com>
2023-10-01 22:57:09 +0500

    Update Neovim config for Neovide

--- nvim/lua/custom/set.lua
+++ nvim/lua/custom/set.lua
@@ -42,10 +42,28 @@ if vim.g.neovide then
     vim.o.guifont = "JetBrainsMono Nerd Font:h10"
     vim.g.neovide_hide_mouse_when_typing = true
     vim.g.neovide_cursor_animation_length = 0
+    vim.g.neovide_refresh_rate_idle = 30
     vim.o.title = true
 
     vim.keymap.set('v', '<C-c>', '"+y')
     vim.keymap.set({'n', 'v'}, '<C-v>', '"+p')
     vim.keymap.set('i', '<C-v>', '<ESC>"+pli')
     vim.keymap.set('c', '<C-v>', '<C-R>+')
+
+    vim.g.neovide_scale_factor = 1.0
+    local change_scale_factor = function(delta)
+        vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+    end
+    vim.keymap.set("n", "<C-=>", function()
+        change_scale_factor(1.25)
+        print("Scale: " .. vim.g.neovide_scale_factor)
+    end)
+    vim.keymap.set("n", "<C-->", function()
+        change_scale_factor(1/1.25)
+        print("Scale: " .. vim.g.neovide_scale_factor)
+    end)
+    vim.keymap.set("n", "<C-0>", function()
+        vim.g.neovide_scale_factor = 1.0
+        print("Scale: " .. vim.g.neovide_scale_factor)
+    end)
 end
