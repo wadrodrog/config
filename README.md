73d23c71329277a040e2720adb74166f34d9d091
wadrodrog <wadrodrog@noreply.codeberg.org>
2023-11-01 21:25:48 +0500

    Add nvim dir restore to restore.sh

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Config Files
 
-This repository contains all my personal config files. These include dotfiles, software settings and docs.
+This repository contains my personal config files: dotfiles, software
+settings and docs.
 
-It may be not very nice as I'm not very experienced user, but feedback is appreciated.
+It may be not very nice as I'm not very experienced user, but feedback is
+appreciated.
+
+## Auto restore
 
 `restore.sh` script automatically restores all symlinks.
 It will remove all original config files and replace with files in repo.
@@ -10,4 +14,5 @@ It is assumed original config files are default or don't exist.
 
 - `~/.config/fish/config.fish` → `./fish/config.fish`
 - `~/.config/fish/fish_plugins` → `./fish/fish_plugins`
+- `~/.config/nvim` → `./nvim`
 - `~/.mozilla/firefox/*-release/chrome/userChrome.css` → `./firefox.css`
--- restore.sh
+++ restore.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# Replacer
 # Make dir, remove original file and create symlink
+# $1: DIR - Directory where the original file is located
+# $2: TRG - File from this repo
+# $3: SYM - Symlink name which is the same as original config file
+
 
 replace() {
-    # Arg $1: DIR
-    # Arg $2: TRG
-    # Arg $3: SYM
     mkdir -p $1
     rm -f $1/$3
     ln -s $PWD/$2 $1/$3
 }
 
-
-# Paths Variables
-# DIR - original directory, SYM - symlink (config place), TRG - target (this repo)
 
 DIR_FISH="$HOME/.config/fish"
 SYM_FISH="config.fish"
@@ -23,13 +20,16 @@ TRG_FISH="fish/config.fish"
 SYM_FISH_PLUGINS="fish_plugins"
 TRG_FISH_PLUGINS="fish/fish_plugins"
 
+DIR_NVIM="$HOME/.config"
+SYM_NVIM="nvim"
+TRG_NVIM="nvim"
+
 DIR_FIREFOX="$HOME/.mozilla/firefox/*-release/chrome"
 SYM_FIREFOX="userChrome.css"
 TRG_FIREFOX="firefox.css"
 
 
-# Replace files
-
 replace $DIR_FISH $TRG_FISH $SYM_FISH
 replace $DIR_FISH $TRG_FISH_PLUGINS $SYM_FISH_PLUGINS
+replace $DIR_NVIM $TRG_NVIM $SYM_NVIM
 replace $DIR_FIREFOX $TRG_FIREFOX $SYM_FIREFOX
